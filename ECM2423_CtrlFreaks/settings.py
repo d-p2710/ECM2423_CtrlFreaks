f8ca14f686539673681225ab95d6c2e3573484cf
Diya Paul <111011417+d-p2710@users.noreply.github.com>
2024-02-19 12:17:07 +0000

    Revert "QR generator."
    This reverts commit bc11ad83e5479525bc070bc7db68a115a6b4c9ad.

--- ECM2423_CtrlFreaks/settings.py
+++ ECM2423_CtrlFreaks/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "ECM2423_CtrlFreaks.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["app/templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('qr-generator/', views.qr_generator, name='qr_generator'),
-    path('qr-scanner/', views.qr_scanner, name='qr_scanner'),
-]
