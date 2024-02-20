e562ab8b9eb14680c9be522071dd38c1add280c4
ZaraJammeh <126472155+ZaraJammeh@users.noreply.github.com>
2024-02-20 00:22:28 +0000

    Fixed issue with url processing for questions in a quiz: - /quiz/x/qy now correctly refers to question y of quiz x - Added "Next" button to question template, which links to next question in the quiz - "Results" button links to end screen and appears instead of Next on last question

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.end, name="end"),
     path("<int:quiz_id>/", views.start, name="start"),
-    path("<int:quiz_id>/q<int:question_id>", views.question, name="question"),
+    path("<int:quiz_id>/q<int:question_index>", views.question, name="question"),
     # path("<int:question_id>/results/", views.results, name="results"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 from .models import Quiz, Question
 
-# Create your views here.
 def start(request, quiz_id):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     return render(request, "quiz/start.html", {"quiz": quiz})
@@ -11,6 +10,14 @@ def start(request, quiz_id):
 def end(request):
     return HttpResponse("End Screen")
 
-def question(request, quiz_id, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "quiz/question.html", {"question": question})
+def question(request, quiz_id, question_index):
+    # Note that question_index starts at 1 for url readability
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    question = get_object_or_404(Question, pk=quiz.question_set.all()[question_index-1].id)
+    # Query equates to "SELECT FROM Question WHERE Question.pk = id of the next question in the quiz"
+    next_q_index = question_index + 1
+    next_url = "/quiz/" + str(quiz_id) + "/q" + str(next_q_index)
+    if next_q_index > quiz.question_set.count():
+        # if on last question, set redirect to end screen
+        next_url = "/quiz"
+    return render(request, "quiz/question.html", {"question": question, "quiz":quiz, "next_url":next_url})
