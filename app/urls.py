29df65d5093ba223a8916fed90345716b5598e07
Amelie <114741088+ar91904@users.noreply.github.com>
2024-02-26 14:08:22 +0000

    Comments

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+#Page URLs
 urlpatterns = [
     path('homePage', views.home_page, name='homePage'),
     path('profile', views.profile, name='profile'),
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+# Renders home page
 def home_page(request):
     return render(request, 'app/homePage.html')
 
+#Renders profile page
 def profile(request):
     return render(request, 'app/profilePage.html')
 # Create your views here.
