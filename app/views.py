9e554ff12ed5bb8d6de4b5000a050564983513a9
tw559 <tw559@exeter.ac.uk>
2024-02-26 15:09:40 +0000

    Updated

--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from PIL import Image
+from io import BytesIO
+import base64
 
 def home_page(request):
     return render(request, 'app/homePage.html')
@@ -6,3 +9,27 @@ def home_page(request):
 def profile(request):
     return render(request, 'app/profilePage.html')
 # Create your views here.
+
+def qr_generator(request):
+    context = {}
+    if request.method == "POST":
+        qr_text = request.POST.get("qr_text", "")
+        qr_image = qrcode.make(qr_text, box_size=15)
+        qr_image_pil = qr_image.get_image()
+        stream = BytesIO()
+        qr_image_pil.save(stream, format='PNG')
+        qr_image_data = stream.getvalue()
+        qr_image_base64 = base64.b64encode(qr_image_data).decode('utf-8')
+        context['qr_image_base64'] = qr_image_base64
+        context['variable'] = qr_text
+    return render(request, 'app/qrGenerator.html', context=context)
+def qr_scanner(request):
+    return render(request, 'app/qrScanner.html')
+
+
+def geolocation_map(request):
+    return render(request, 'app/geolocationMap.html')
+
+
+def game_map(request):
+    return render(request, 'app/gameMap.html')
