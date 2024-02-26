8fbf38a149bce63314eac7d2db3211d4f5abb90d
tw559 <126590494+tw559@users.noreply.github.com>
2024-02-26 13:10:16 +0000

    Add files via upload

--- ECM2423_CtrlFreaks/urls.py
+++ ECM2423_CtrlFreaks/urls.py
@@ -25,6 +25,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
+    path('quiz/', include('quiz.urls')),
     path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
                                            authentication_form=LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
