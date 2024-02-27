1d928ce3a07b166f893903801d0ec2d08eccd6f2
ZaraJammeh <126472155+ZaraJammeh@users.noreply.github.com>
2024-02-27 12:25:43 +0000

    Fixed backend form processing: - Score is calculated from request.POST - Quiz summary is displayed on results page (html template) - Answers are bold if the user selected them (css) - Answers are red if incorrect or green if correct (css)

--- quiz/views.py
+++ quiz/views.py
@@ -13,20 +13,24 @@ def questions(request, quiz_id):
     return render(request, "quiz/questions.html", {"quiz": quiz})
 
 def results(request, quiz_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
     # process POST request input
-    results_data = {}
     request_as_dict = request.POST.dict()
+    del request_as_dict["csrfmiddlewaretoken"]
+    results_data = {}
+    answer_id_list = []
     # group answers by question id
-    # for key, answer_id in request_as_dict:
-    #     question_id = key.split("-")[0]
-    #     if question_id not in results_data.keys():
-    #         results_data[question_id] = [] 
-    #     results_data[question_id].append(answer_id)
-    # del results_data["csrfmiddlewaretoken"]
+    for entry in request_as_dict:
+        question_id = entry.split("-")[0]
+        if question_id not in results_data.keys():
+            results_data[question_id] = [] 
+        results_data[question_id].append(request_as_dict[entry])
+        answer_id_list.append(int(request_as_dict[entry]))
     score = 0
     # calculate awarded points and save to user DB
-    for question_id, answer_ids in results_data:
+    for question_id in results_data:
         #TODO decide on scoring system for different types of questions
+        answer_ids = results_data[question_id]
         question = get_object_or_404(Question, pk=int(question_id))
         if question.question_type == "SIN":
             # award a flat 5 marks if user gave the question's only correct answer
@@ -38,10 +42,10 @@ def results(request, quiz_id):
             correct_answers = 0
             for answer_id in answer_ids:
                 answer = get_object_or_404(Answer, pk=int(answer_id))
-                if answer.is_correct == True:
+                if answer.is_correct:
                     correct_answers += 1
-            if correct_answers / question.answer_set.count() == 1:
+            if correct_answers / question.answer_set.filter(is_correct=True).count() == 1:
                 score += 10
-            elif correct_answers / question.answer_set.filter(is_correct=True).count() > 0.7:
+            elif correct_answers / question.answer_set.filter(is_correct=True).count() > 0.4:
                 score += 5
-    return HttpResponse("Results page - work in progress")
+    return render(request, "quiz/results.html", {"quiz": quiz, "user_answers": answer_id_list})
