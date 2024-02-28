be827673227a6616ac08b19263441c87bb1351a2
ZaraJammeh <126472155+ZaraJammeh@users.noreply.github.com>
2024-02-28 23:11:12 +0000

    Update quiz with css by ar919 and results code (zj274)

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 app_name = "quiz"
 urlpatterns = [
-    path("", views.end, name="end"),
-    path("<int:quiz_id>/", views.start, name="start"),
+    path("<int:quiz_id>/start", views.start, name="start"),
     path("<int:quiz_id>/q<int:question_index>", views.question, name="question"),
-    # path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:quiz_id>/results", views.results, name="results"),
 ]
