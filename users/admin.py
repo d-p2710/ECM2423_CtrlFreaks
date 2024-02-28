28cf2dd0b5e65895273d401e9a7f4da5fc186aaa
ZaraJammeh <126472155+ZaraJammeh@users.noreply.github.com>
2024-02-28 21:43:18 +0000

    Avatar Parts Functionality - Add models for Avatar and Avatar parts - Allow avatar parts to be uploaded to the datebase by admins - Create and link default avatar for user automatically when new user is created

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Profile
+from .models import AvatarPart, Avatar
 
 # Allows admins to read and modify user profiles
 admin.site.register(Profile)
+# Allow admins to upload and edit avatar images
+admin.site.register([AvatarPart, Avatar])
