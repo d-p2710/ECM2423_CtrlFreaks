2e8821fa46bd10533353da5732ae9a717d7eeed8
tylda <114699542+mm1224@users.noreply.github.com>
2024-02-28 16:53:03 +0000

    changes to style.css profilePage and models

--- users/models.py
+++ users/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from ECM2423_CtrlFreaks import settings
+
+
 # Creates a separate object that's linked to the user which stores additional information
 class Profile(models.Model):
     # Model is one to one, so each one user links to one profile
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    avatar = models.FileField(default='Example_1.png', upload_to='profile_images')
     bio = models.TextField()
     points_amount = models.IntegerField(default=0)
 
