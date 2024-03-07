049ef47e16b583c86cd5285923702bbaff520150
tw559 <tw559@exeter.ac.uk>
2024-03-07 14:30:21 +0000

    - Question form now shows a 'Next' button if there's another question, otherwise it shows a 'Results' button. - Results button doesn't actually pass the user to the results page for the question though.

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.urls import reverse
+from django.core.paginator import Paginator
 
 from .models import Quiz, Question, Answer
 
@@ -13,14 +14,25 @@ def question(request, quiz_id, question_index):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     question = get_object_or_404(Question, pk=quiz.question_set.all()[question_index-1].id)
     # Query equates to "SELECT FROM Question WHERE Question.pk = id of the next question in the quiz"
-    next_q_index = question_index + 1
+
+    page = request.GET.get('page')
+
+    question_next = question_index + 1
     next_url = ""
-    if next_q_index > quiz.question_set.count():
+    if question_next > quiz.question_set.count():
         # if on last question, set redirect to end screen
         next_url =  reverse("quiz:results", args=[quiz_id])
     else:
-        next_url = reverse("quiz:question", args=(quiz_id, next_q_index))
-    return render(request, "quiz/question.html", {"question": question, "quiz":quiz, "next_url":next_url})
+        next_url = reverse("quiz:question", args=(quiz_id, question_next))
+
+    context = {
+        "question": question,
+        "quiz": quiz,
+        "next_url": next_url,
+        "question_next": question_next,
+    }
+
+    return render(request, "quiz/question.html", context)
 
 def results(request, quiz_id):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
