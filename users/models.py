10fc4f2177d3d4570b34d13cd2b087c4c8bcf48b
ZaraJammeh <126472155+ZaraJammeh@users.noreply.github.com>
2024-03-15 14:07:10 +0000

    Manual merge for zj274-quiz-patch into copy of master

--- users/models.py
+++ users/models.py
@@ -9,14 +9,14 @@ class Profile(models.Model):
     # avatar = models.ForeignKey("Avatar", on_delete=models.CASCADE)
     bio = models.TextField()
     points_amount = models.IntegerField(default=0)
-
+    coins_amount = models.IntegerField(default=0)
     def __str__(self):
         return self.user.username
 
 # zj274
 # Create database models for storing and fetching avatar images
-class AvatarPart(models.Model):
-    PART_TYPES = {
+class Item(models.Model):
+    CATEGORIES = {
         "colour": "Colour",
         "mouth": "Mouth",
         "eyes": "Eyes",
@@ -24,34 +24,37 @@ class AvatarPart(models.Model):
         "accessory": "Accessory",
     }
     name = models.CharField(max_length=200)
-    part_type = models.CharField(max_length=200, choices=PART_TYPES)
+    category = models.CharField(max_length=200, choices=CATEGORIES)
+    price = models.IntegerField()
     img_file = models.ImageField("Image file", upload_to='profile_images')
-    is_default_img = models.BooleanField("Default (image is default selection for this avatar part)", default=False)
+    is_default_img = models.BooleanField("Default (image is default selection for this item)", default=False)
     def __str__(self):
         return self.name
 
 class Avatar(models.Model):
     # links a profile to the user's selected avatar parts
     profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    colour = models.ForeignKey(AvatarPart, null=True,
-                                on_delete=models.CASCADE,
-                                limit_choices_to={"part_type": "colour"},
+    colour = models.ForeignKey(Item, on_delete=models.CASCADE,
+                                limit_choices_to={"category": "colour"},
                                 related_name="+",)
-    mouth = models.ForeignKey(AvatarPart, null=True,
-                                on_delete=models.CASCADE,
-                                limit_choices_to={"part_type": "mouth"},
+    mouth = models.ForeignKey(Item, on_delete=models.CASCADE,
+                                limit_choices_to={"category": "mouth"},
                                 related_name="+",)
-    eyes = models.ForeignKey(AvatarPart, null=True,
-                                on_delete=models.CASCADE,
-                                limit_choices_to={"part_type": "eyes"},
+    eyes = models.ForeignKey(Item, on_delete=models.CASCADE,
+                                limit_choices_to={"category": "eyes"},
                                 related_name="+",)
-    headwear = models.ForeignKey(AvatarPart, null=True,
+    headwear = models.ForeignKey(Item, null=True,
                                 on_delete=models.CASCADE,
-                                limit_choices_to={"part_type": "headwear"},
+                                limit_choices_to={"category": "headwear"},
                                 related_name="+",)
-    accessory = models.ForeignKey(AvatarPart, null=True,
+    accessory = models.ForeignKey(Item, null=True,
                                 on_delete=models.CASCADE,
-                                limit_choices_to={"part_type": "accessory"},
+                                limit_choices_to={"category": "accessory"},
                                 related_name="+",)
     def __str__(self):
         return self.profile.user.username
+    
+class OwnedItem(models.Model):
+    # resolves many-to-many relationship between User/Profile and AvatarPart
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
