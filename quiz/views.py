e5b47216571bb3e576bb5b2e804b20bd19a1d76f
ZaraJammeh <126472155+ZaraJammeh@users.noreply.github.com>
2024-03-17 14:49:33 +0000

    Added Leaderboard elements to home page + made fixes to quiz - Leaderboard showing usernames and points of top 10 points-earning profiles is now displayed on home page - In the case that there are < 10 profiles with more than 0 points, leaderboard slot(s) is/are left blank - Top 3 "podiums" are now displayed on home page, showing points, username and avatar for each of the top 3 profiles - All quiz questions are now shown on a single page - Prevented rewarding coins and points for refresh spam on results page by adding ajax to quiz "results" button
    Known Issues:
    - Lack of css stylisation for new additions to home page
    - Users can still exploit loop of pressing browser's back button on results page and results button on quiz page
    to gain points and coins as the form retains the selected answers
    Potential fix suggestions:
      a) Force clear form when page is displayed
      b) Implement (daily/weekly) limit on how many times a user can attempt a quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,52 +1,33 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
+import json
 
 from .models import Quiz, Question, Answer
+from users.models import Profile
 
-def start(request, quiz_id):
-    quiz = get_object_or_404(Quiz, pk=quiz_id)
-    return render(request, "quiz/start.html", {"quiz": quiz})
-
-def question(request, quiz_id, question_index):
-    # question_index starts at 1 for url readability
-    quiz = get_object_or_404(Quiz, pk=quiz_id)
-    question = get_object_or_404(Question, pk=quiz.question_set.all()[question_index-1].id)
-    # Query equates to "SELECT FROM Question WHERE Question.pk = id of the next question in the quiz"
-    next_q_index = question_index + 1
-    next_url = ""
-    if next_q_index > quiz.question_set.count():
-        # if on last question, set redirect to end screen
-        next_url =  reverse("quiz:results", args=[quiz_id])
-    else:
-        next_url = reverse("quiz:question", args=(quiz_id, next_q_index))
-    return render(request, "quiz/question.html", {"question": question, "quiz":quiz, "next_url":next_url})
-
-def results(request, quiz_id):
-    quiz = get_object_or_404(Quiz, pk=quiz_id)
-    # process POST request input
-    request_as_dict = request.POST.dict()
-    del request_as_dict["csrfmiddlewaretoken"]
-    results_data = {}
-    answer_id_list = []
+# auxiliary functions
+def format_answers(answers_data):
     # group answers by question id
-    for entry in request_as_dict:
+    formatted_answers = {}
+    for entry in answers_data:
         question_id = entry.split("-")[0]
-        if question_id not in results_data.keys():
-            results_data[question_id] = [] 
-        results_data[question_id].append(request_as_dict[entry])
-        answer_id_list.append(int(request_as_dict[entry]))
+        if question_id not in formatted_answers.keys():
+            formatted_answers[question_id] = [] 
+        formatted_answers[question_id].append(answers_data[entry])
+    return formatted_answers
+
+def calculate_score(results_dict):
     score = 0
-    # calculate awarded points and save to user DB
-    for question_id in results_data:
+    for question_id in results_dict:
         #TODO decide on scoring system for different types of questions
-        answer_ids = results_data[question_id]
+        answer_ids = results_dict[question_id]
         question = get_object_or_404(Question, pk=int(question_id))
         if question.question_type == "SIN":
-            # award a flat 5 marks if user gave the question's only correct answer
+            # award a flat 2 marks if user gave the question's only correct answer
             answer = get_object_or_404(Answer, pk=int(answer_ids[0]))
             if answer.is_correct == True:
-                score += 5
+                score += 2
         elif question.question_type == "OOM":
             # award marks based on proportion of correct answers
             correct_answers = 0
@@ -55,8 +36,53 @@ def results(request, quiz_id):
                 if answer.is_correct:
                     correct_answers += 1
             if correct_answers / question.answer_set.filter(is_correct=True).count() == 1:
-                score += 10
-            elif correct_answers / question.answer_set.filter(is_correct=True).count() > 0.4:
-                score += 5
-    return render(request, "quiz/results.html", {"quiz": quiz, "user_answers": answer_id_list})
+                score += 3
+            elif correct_answers / question.answer_set.filter(is_correct=True).count() > 0.65:
+                score += 1
+    return score
+
+def start(request, quiz_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    return render(request, "quiz/start.html", {"quiz": quiz})
+
+def question(request, quiz_id):
+    # question_index starts at 1 for url readability
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    return render(request, "quiz/question.html", {"quiz":quiz})
+
+def post_answers(request):
+    # dedicated backend view to process quiz rewards - prevents refreshing and resubmitting for rewards
+    if request.method == "POST":
+        # process POST request input
+        post_dict = json.load(request)
+        results_dict = format_answers(post_dict)
+        score = calculate_score(results_dict)
+        # save points and coins to database
+        points = score * 100
+        marker = request.user.id
+        profile = Profile.objects.get(user=marker)
+        profile.points_amount += points
+        profile.coins_amount += score
+        profile.save()
+        return HttpResponse("success")
+
+def results(request, quiz_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    if request.method == "POST":
+        # process POST request input
+        request_as_dict = request.POST.dict()
+        del request_as_dict["csrfmiddlewaretoken"]
+        results_dict = format_answers(request_as_dict)
+        answer_id_list = []
+        for answer_list in results_dict.values():
+            for answer_id in answer_list:
+                answer_id_list.append(int(answer_id))        
+        score = calculate_score(results_dict)
+        context = {
+                    "quiz": quiz,
+                    "user_answers": answer_id_list,
+                    "points": score * 100,
+                    "coins": score,
+                }
+        return render(request, "quiz/results.html", context)
 
